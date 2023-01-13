ee20b49e067ee87da92b7067b6592249132d936a
Bobby <a0930023855@gmail.com>
2023-01-13 17:56:57 +0800

    上傳與顯示圖片

--- UserSystemSB/src/main/java/com/example/controller/JsonController.java
+++ UserSystemSB/src/main/java/com/example/controller/JsonController.java
@@ -1,20 +1,49 @@
 package com.example.controller;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.tomcat.util.file.ConfigurationSource.Resource;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.model.User;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @CrossOrigin
 @Controller
@@ -95,5 +124,48 @@ public class JsonController {
 		System.out.println("資料: "+params);
 		return "postUser2--->請求成功"; 
 	}
+	
+	
+	//下載post過來的圖片
+	@PostMapping("/postimg")
+	@ResponseBody
+	public String postimg(@RequestParam(name = "image") MultipartFile multipartFile) throws IOException {
+		Path uploadPath = Paths.get("C:/Users/USER/Desktop/images/");
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+         
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve("test.png");
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {        
+            throw new IOException("Could not save image file");
+        }
+        
+		return "上傳成功";
+	}
+	
+	//使前端網頁可以透過href取得圖片位置
+    @RequestMapping(value = "/getimg", method = RequestMethod.GET,
+            produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> getImage() throws IOException {
+    	FileSystemResource imgFile2 = new FileSystemResource("C:/Users/USER/Desktop/images/test.png");
+        //var imgFile = new ClassPathResource("static/person.png");
+        byte[] bytes = StreamUtils.copyToByteArray(imgFile2.getInputStream());
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(bytes);
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
